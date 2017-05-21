ae0bde55ba280b2c2868ffaffd2b203440de1975
dnaiel <dog830228@gmail.com>
2017-05-21 11:24:02 +0800

    init

---
+++ js/WeatherScript.js
@@ -0,0 +1,84 @@
+﻿/// <reference path=”http://code.jquery.com/jquery-1.12.4.min.js” />
+/*
+create by daniel 2017/02/23
+*/
+var cityLenght = 0;//多少城市
+var i = 0;
+//設置天氣圖
+function SetWeeaTher(woeid) {
+    //將locationQuery不要轉換
+    var locationQuery = escape("select * from weather.forecast where woeid ='" + woeid + "' and u='c'");
+    $.getJSON("https://query.yahooapis.com/v1/public/yql?q=" + locationQuery + "&format=json",
+        function (data) {
+            var forcast = data.query.results.channel.item.forecast;//本周天氣
+            var cityName = word[0].City[i].CName//城市
+			$("#CName").text(cityName);
+			Setforcast(forcast);
+			i++;
+        });
+}
+
+function Setforcast(forcast) {
+	$(".weatherNow").empty();
+	$(".weatherWeek").empty();
+	for(index = 0; index < 7; index++){
+		if (index == 0) {
+			NowWeather(forcast[index]);
+        }
+		weatherWeekDay(forcast[index],index);	
+	}
+}
+//今天氣象
+function NowWeather(data){
+	//天氣平均溫度
+	var averagetemp=(parseInt(data.high)+parseInt(data.low))/2;
+	var $WeatherNow='<div class="weatherNowIcon">'+
+						'<img src="images/'+weather_con[data.code][0]+'" />'+//天氣編號
+					'</div>'+
+					'<div class="weatherNowDetail">'+
+						'<h2>'+weather_con[data.code][1]+'</h2>'+
+						'<p>'+data.low+'&deg; / '+data.high+'&deg;</p>'+//最高  最低溫
+					'</div>'+
+					'<div class="weatherNowTemp">'+
+						'<p>'+parseInt(averagetemp)+'&deg;</p>'+
+					'</div>';
+	$(".weatherNow").append($WeatherNow);							
+}
+//下一周氣象
+function weatherWeekDay(data,index){
+	//如果是下周的頭和尾
+	var signoe='';
+	if(index==0){
+		signoe=' weatherWeekDayFirst';		
+	}
+	else if(index==6){
+		signoe=' weatherWeekDayLast';		
+	}
+	//天氣平均溫度
+	var averagetemp=(parseInt(data.high)+parseInt(data.low))/2;
+	var $Weather='<div class="weatherWeekDay'+signoe+'">'+
+						'<p>'+WeekChinese[data.day]+'</p>'+//星期幾
+						'<div class="weatherWeekIcon">'+
+							'<img src="images/'+weather_con[data.code][0]+'" />'+//天氣編號
+						'</div>'+
+						'<p>'+parseInt(averagetemp)+'&deg;</p>'+
+					'</div>';
+	$(".weatherWeek").append($Weather);							
+}
+//計算有幾個城市
+(function () {
+    $.each(word[0].City, function (index, value) {
+        cityLenght++;
+    });
+})();
+//每一秒跑一張圖 大於目前城鎮數量從0開始算
+function ShowWaeather() {
+    if (i>=cityLenght) {
+        i = 0;
+    }
+    $("#todayWeather").empty();
+    $("#otherdayWeather").empty();
+    SetWeeaTher(word[0].City[i].ID);
+    setTimeout("ShowWaeather()", 5000);
+};
+ShowWaeather();
---
+++ js/cityData.js
@@ -0,0 +1,73 @@
+﻿var word = [
+   {
+	   Area: 'TW', City: [
+		  { ID: '20070568',CName: '台北', EName: 'Taipei' },
+	      { ID: '2347336', CName: '宜蘭', EName: 'Yilan' },
+		  { ID: '2306206', CName: '嘉義', EName: 'Chiayi' },
+		  { ID: '2306190', CName: '台東', EName: 'Taitung' },
+		  { ID: '2306181', CName: '台中', EName: 'Taichung' }
+	   ]
+   }
+];
+
+var WeekChinese={
+	"Mon":"一",
+	"Tue":"二",
+	"Wed":"三",
+	"Thu":"四",
+	"Fri":"五",
+	"Sat":"六",
+	"Sun":"日"
+}
+
+var weather_con = {
+	"0":["weather_cloudy.png","多雲"],
+	"1":["weather_cloudy.png","多雲"],
+	"2":["weather_rain.png","雨天"],
+	"3":["weather_thunder.png","雷雨"],
+	"4":["weather_thunder.png","雷雨"],
+	"5":["weather_rain.png","雨天"],
+	"6":["weather_rain.png","雨天"],
+	"7":["weather_rain.png","雨天"],
+	"8":["weather_rain.png","雨天"],
+	"9":["weather_rain.png","雨天"],
+	"10":["weather_rain.png","雨天"],
+	"11":["weather_rain.png","雨天"],
+	"12":["weather_rain.png","雨天"],
+	"13":["weather_rain.png","雨天"],
+	"14":["weather_rain.png","雨天"],
+	"15":["weather_rain.png","雨天"],
+	"16":["weather_rain.png","雨天"],
+	"17":["weather_rain.png","雨天"],
+	"18":["weather_rain.png","雨天"],
+	"19":["weather_cloudy.png","多雲"],
+	"20":["weather_cloudy.png","多雲"],
+	"21":["weather_cloudy.png","多雲"],
+	"22":["weather_cloudy.png","多雲"],
+	"23":["weather_cloudy.png","多雲"],
+	"24":["weather_cloudy.png","多雲"],
+	"25":["weather_cloudy.png","多雲"],
+	"26":["weather_cloudy.png","多雲"],
+	"27":["weather_cloudy.png","多雲"],
+	"28":["weather_cloudy.png","多雲"],
+	"29":["weather_cloudy.png","多雲"],
+	"30":["weather_cloudy.png","多雲"],
+	"31":["weather_sunny.png","晴天"],
+	"32":["weather_sunny.png","晴天"],
+	"33":["weather_sunny.png","晴天"],
+	"34":["weather_sunny.png","晴天"],
+	"35":["weather_cloudy.png","多雲"],
+	"36":["weather_sunny.png","晴天"],
+	"37":["weather_thunder.png","雷雨"],
+	"38":["weather_cloudy.png","多雲"],
+	"39":["weather_cloudy.png","多雲"],
+	"40":["weather_cloudy.png","多雲"],
+	"41":["weather_rain.png","雨天"],
+	"42":["weather_rain.png","雨天"],
+	"43":["weather_rain.png","雨天"],
+	"44":["weather_cloudy.png","多雲"],
+	"45":["weather_cloudy.png","多雲"],
+	"46":["weather_rain.png","雨天"],
+	"47":["weather_cloudy.png","多雲"],
+	"3200":"資料錯誤"
+}
